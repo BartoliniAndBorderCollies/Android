36251679e2db7fe5da3152b7ec79627524058ef3
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-08-13 10:08:20 +0200

    add TaskDatabase configuration

--- app/src/main/java/com/klodnicki/taskmanager/data/TaskDatabase.java
+++ app/src/main/java/com/klodnicki/taskmanager/data/TaskDatabase.java
@@ -1,4 +1,35 @@
 package com.klodnicki.taskmanager.data;
 
-public class TaskDatabase {
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.klodnicki.taskmanager.data.dao.StatusDao;
+import com.klodnicki.taskmanager.data.dao.TagDao;
+import com.klodnicki.taskmanager.data.dao.TaskDao;
+import com.klodnicki.taskmanager.data.entity.Status;
+import com.klodnicki.taskmanager.data.entity.Tag;
+import com.klodnicki.taskmanager.data.entity.Task;
+
+@Database(entities = {Status.class, Tag.class, Task.class}, version = 1)
+public abstract class TaskDatabase extends RoomDatabase {
+    public StatusDao statusDao;
+    public TagDao tagDao;
+    public TaskDao taskDao;
+
+    private static volatile TaskDatabase INSTANCE;
+
+    public static TaskDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (TaskDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            TaskDatabase.class, "task_database").build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
 }
