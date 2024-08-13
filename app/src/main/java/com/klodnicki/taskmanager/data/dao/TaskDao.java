6ba4848088a21fec20220ecf7294b2569e170fcf
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-08-13 10:07:41 +0200

    add TaskDao interface

--- app/src/main/java/com/klodnicki/taskmanager/data/dao/TaskDao.java
+++ app/src/main/java/com/klodnicki/taskmanager/data/dao/TaskDao.java
@@ -1,4 +1,32 @@
 package com.klodnicki.taskmanager.data.dao;
 
-public class TaskDao {
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.klodnicki.taskmanager.data.entity.Task;
+
+import java.util.List;
+import java.util.UUID;
+
+import kotlinx.coroutines.flow.Flow;
+
+@Dao
+public interface TaskDao {
+    @Query("SELECT * FROM task_table")
+    Flow<List<Task>> findAll();
+
+    @Query("SELECT * FROM task_table WHERE id = :id")
+    Task findById(UUID id);
+
+    @Insert
+    void create(Task task);
+
+    @Update
+    void update(Task updatedTask);
+
+    @Delete
+    void deleteById(Task task);
 }
