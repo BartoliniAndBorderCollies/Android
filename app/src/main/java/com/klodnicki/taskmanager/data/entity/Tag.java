5788c955e0d5800f45a1f7a005cc5ca3d71813d8
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-08-13 10:08:49 +0200

    refactor of Tag class

--- app/src/main/java/com/klodnicki/taskmanager/data/entity/Tag.java
+++ app/src/main/java/com/klodnicki/taskmanager/data/entity/Tag.java
@@ -1,26 +1,16 @@
 package com.klodnicki.taskmanager.data.entity;
 
+
 import androidx.annotation.NonNull;
-import androidx.room.ColumnInfo;
 import androidx.room.Entity;
-import androidx.room.ForeignKey;
-import androidx.room.Index;
 import androidx.room.PrimaryKey;
 
 import java.util.UUID;
 
-@Entity(tableName = "tag_table", foreignKeys = @ForeignKey(entity = Task.class,
-        parentColumns = "id",
-        childColumns = "task_id",
-        onDelete = ForeignKey.CASCADE), indices = {@Index(value = {"task_id"})})
+@Entity(tableName = "tag_table")
 public class Tag {
-
     @PrimaryKey
     @NonNull
-    private UUID id;  // Primary Key for Tag entity
-
-    @NonNull
-    @ColumnInfo(name = "task_id")
-    private UUID taskId;
-
+    private UUID id;
+    private String name;
 }
