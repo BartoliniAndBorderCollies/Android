e06849f486ac2b49bc1263017904c24b011139c9
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-08-13 10:06:26 +0200

    add structure

--- app/src/main/java/com/klodnicki/taskmanager/data/entity/Status.java
+++ app/src/main/java/com/klodnicki/taskmanager/data/entity/Status.java
@@ -1,4 +1,16 @@
 package com.klodnicki.taskmanager.data.entity;
 
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.util.UUID;
+
+@Entity(tableName = "status_table")
 public class Status {
+    @PrimaryKey
+    @NonNull
+    private UUID id;
+
+    private String name;
 }
