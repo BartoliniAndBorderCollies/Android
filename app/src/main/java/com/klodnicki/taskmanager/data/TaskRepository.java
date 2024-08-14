f4f6dea68f11d177b960ee071f08fbb9eb597c5e
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-08-14 12:46:42 +0200

    add TaskRepository with all basic methods

--- app/src/main/java/com/klodnicki/taskmanager/data/TaskRepository.java
+++ app/src/main/java/com/klodnicki/taskmanager/data/TaskRepository.java
@@ -1,4 +1,59 @@
 package com.klodnicki.taskmanager.data;
 
+import com.klodnicki.taskmanager.data.dao.StatusDao;
+import com.klodnicki.taskmanager.data.dao.TagDao;
+import com.klodnicki.taskmanager.data.dao.TaskDao;
+import com.klodnicki.taskmanager.data.entity.Status;
+import com.klodnicki.taskmanager.data.entity.Tag;
+import com.klodnicki.taskmanager.data.entity.Task;
+
+import java.util.List;
+import java.util.UUID;
+
+import kotlinx.coroutines.flow.Flow;
+
 public class TaskRepository {
+
+    private final StatusDao statusDao;
+
+    private final TagDao tagDao;
+
+    private final TaskDao taskDao;
+
+    private final Flow<List<Task>> allTasks;
+
+    public TaskRepository(StatusDao statusDao, TagDao tagDao, TaskDao taskDao) {
+        this.statusDao = statusDao;
+        this.tagDao = tagDao;
+        this.taskDao = taskDao;
+        allTasks = taskDao.findAll();
+    }
+
+    Flow<List<Task>> getAllTasks() {
+        return allTasks;
+    }
+
+    void insert(Task task) {
+        taskDao.create(task);
+    }
+
+    void update(Task task) {
+        taskDao.update(task);
+    }
+
+    void delete(Task task) {
+        taskDao.delete(task);
+    }
+
+    Task getTaskById(UUID id) {
+        return taskDao.findById(id);
+    }
+
+    List<Tag> getTagsForTask(UUID taskId) {
+        return tagDao.getTagsForTask(taskId);
+    }
+
+    Status getStatusById(UUID id) {
+        return statusDao.findById(id);
+    }
 }
